5a817c6a3f9e8de0451ec3dc5d36efe9c10c1100
Soumyadeep Datta <sdatta@nyu.edu>
2023-08-09 20:07:23 +0530

    Baby step to multi-antenna mmW UE

--- Simulation/function_monte_carlo_mmW_only.m
+++ Simulation/function_monte_carlo_mmW_only.m
@@ -31,12 +31,12 @@ for k = 1:K_mmW
                 warning('off','all');
                 for kk = 1:K_mmW
                 %----------MONTE-CARLO
-                    HI_UE_rx_monte_mmW(k,ch,ap,kk) = K_UE_RX*(eta(ap,kk)*(h_mmW(:,:,ch,ap,k)' *(h_hat_mmW(:,:,ch,ap,kk)*h_hat_mmW(:,:,ch,ap,kk)'*h_mmW(:,:,ch,ap,k))) ...
-                        + K_AP_TR*eta(ap,kk)*(h_mmW(:,:,ch,ap,k)'*diag(diag(h_hat_mmW(:,:,ch,ap,kk)*h_hat_mmW(:,:,ch,ap,kk)'))*h_mmW(:,:,ch,ap,k)));   
+                    HI_UE_rx_monte_mmW(k,ch,ap,kk) = K_UE_RX*eta(ap,kk)*sum(sum((h_mmW(:,:,ch,ap,k)' *(h_hat_mmW(:,:,ch,ap,kk)*h_hat_mmW(:,:,ch,ap,kk)'*h_mmW(:,:,ch,ap,k))),2),1) ...
+                        + K_AP_TR*eta(ap,kk)*sum(sum((h_mmW(:,:,ch,ap,k)'*diag(diag(h_hat_mmW(:,:,ch,ap,kk)*h_hat_mmW(:,:,ch,ap,kk)'))*h_mmW(:,:,ch,ap,k)),2),1);   
                 end
                 HI_AP_variance_mmW = HI_AP_variance_mmW + K_AP_TR*eta(ap,kk)*diag(diag(h_hat_mmW(:,:,ch,ap,kk)*h_hat_mmW(:,:,ch,ap,kk)')); % sum over UEs -- a-th AP HW 
 %                 HI_AP_tx_monte(k,ch,ap,kk) =K_AP_TR*eta(ap,kk)*(h(:,ch,ap,k)'*diag(diag(h_hat(:,ch,ap,kk)*h_hat(:,ch,ap,kk)'))*h(:,ch,ap,k)); %closed-frominsome
-                HI_AP_tx_monte_mmW(k,ch) = HI_AP_tx_monte_mmW(k,ch) + (h_mmW(:,:,ch,ap,k)'* sqrtm(HI_AP_variance_mmW)*sqrt(0.5)*(randn(N_mmW,1)+1i*randn(N_mmW,1)) ); %sum over APs
+                HI_AP_tx_monte_mmW(k,ch) = HI_AP_tx_monte_mmW(k,ch) + sum(h_mmW(:,:,ch,ap,k)'* sqrtm(HI_AP_variance_mmW)*sqrt(0.5)*(randn(N_mmW,1)+1i*randn(N_mmW,1))); %sum over APs
             end
             HI_UE_RX_MC1 = sum(sum(HI_UE_rx_monte_mmW,4),3); 
             HI_UE_rx_m1(ch) = abs( sqrt(0.5*HI_UE_RX_MC1(k,ch))*(randn(1,1)+1i*randn(1,1)))^2;
@@ -65,7 +65,7 @@ for k = 1:K_mmW
                 interference_kj_monte =0;
                 for ap2 = 1:L
                     % interference_kj_monte = interference_kj_monte + sqrt(eta(ap2,kd))*h(:,ch,ap2,k)'*h_hat(:,ch,ap2,kd);
-                    interference_kj_monte = interference_kj_monte + sqrt(eta(ap2,kd))*h(:,ch,ap2,k-K_mmW)'*h_hat(:,ch,ap2,kd-K_mmW);
+                    interference_kj_monte = interference_kj_monte + sqrt(eta(ap2,kd))*sum(h_mmW(:,:,ch,ap2,k)'*h_hat(:,ch,ap2,kd-K_mmW));
                 end
                 interference_kj_monte = abs(interference_kj_monte)^2;
                 interference_kj_monte_sum = interference_kj_monte_sum + interference_kj_monte;
