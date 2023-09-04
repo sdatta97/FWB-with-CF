af2306027df930e322812778722f3215dc432cf6
sdatta97 <sdatta@nyu.edu>
2023-09-04 12:31:09 +0530

    Adding Psi' matrices

--- Simulation/functionComputeExpectationsv3.m
+++ Simulation/functionComputeExpectationsv3.m
@@ -76,17 +76,19 @@ for n=1:nbrOfRealizations
     D_sub6_sub6 = zeros(K-K_mmW,K-K_mmW,N_UE_sub6,N_UE_sub6);
     Psi_mmW = zeros(K_mmW,N_UE_mmW,N_UE_mmW);
     Psi_sub6 = zeros(K-K_mmW,N_UE_sub6,N_UE_sub6);
+    Psi_mmW_2 = zeros(K_mmW,N_UE_mmW,N_UE_mmW);
+    Psi_sub6_2 = zeros(K-K_mmW,N_UE_sub6,N_UE_sub6);
     for k = 1:K_mmW
         Psi_mmW (k,:,:) = eye(N_UE_mmW);
         for i = 1:K_mmW
+            for l = 1:L
+                H = reshape(H_mmW((l-1)*N+1:l*N,n,:,k), [N,N_UE_mmW]);
+                Hhat = reshape(Hhat_mmW((l-1)*N+1:l*N,n,:,k), [N,N_UE_mmW]);
+                H_int = reshape(H_mmW((l-1)*N+1:l*N,n,:,i), [N,N_UE_mmW]);
+                Hhat_int = reshape(Hhat_mmW((l-1)*N+1:l*N,n,:,i), [N,N_UE_mmW]);
+                D_mmW_mmW(k,i,:,:) = reshape(D_mmW_mmW(k,i,:,:),[N_UE_mmW,N_UE_mmW]) + H'*H_int;
+            end
             if (i~=k)
-                for l = 1:L
-                    H = reshape(H_mmW((l-1)*N+1:l*N,n,:,k), [N,N_UE_mmW]);
-                    Hhat = reshape(Hhat_mmW((l-1)*N+1:l*N,n,:,k), [N,N_UE_mmW]);
-                    H_int = reshape(H_mmW((l-1)*N+1:l*N,n,:,i), [N,N_UE_mmW]);
-                    Hhat_int = reshape(Hhat_mmW((l-1)*N+1:l*N,n,:,i), [N,N_UE_mmW]);
-                    D_mmW_mmW(k,i,:,:) = reshape(D_mmW_mmW(k,i,:,:),[N_UE_mmW,N_UE_mmW]) + H'*H_int;
-                end
                 Psi_mmW (k,:,:) = reshape(Psi_mmW(k,:,:),[N_UE_mmW,N_UE_mmW]) + p(k)*reshape(D_mmW_mmW(k,i,:,:),[N_UE_mmW,N_UE_mmW])*reshape(D_mmW_mmW(k,i,:,:),[N_UE_mmW,N_UE_mmW])';
             end
         end
@@ -100,7 +102,9 @@ for n=1:nbrOfRealizations
             end
             Psi_mmW (k,:,:) = reshape(Psi_mmW(k,:,:),[N_UE_mmW,N_UE_mmW]) + p(k)*reshape(D_mmW_sub6(k,i,:,:),[N_UE_mmW,N_UE_sub6])*reshape(D_mmW_sub6(k,i,:,:),[N_UE_mmW,N_UE_sub6])';
         end
+        Psi_mmW_2(k,:,:) = inv(reshape(Psi_mmW(k,:,:),[N_UE_mmW,N_UE_mmW])) + reshape(D_mmW_mmW(k,k,:,:),[N_UE_mmW,N_UE_mmW])*reshape(D_mmW_mmW(k,k,:,:),[N_UE_mmW,N_UE_mmW])';
     end
+
     for k = 1:K-K_mmW   
         Psi_sub6 (k,:,:) = eye(N_UE_sub6);
         for i = 1:K_mmW
@@ -114,17 +118,18 @@ for n=1:nbrOfRealizations
             Psi_sub6 (k,:,:) = reshape(Psi_sub6(k,:,:),[N_UE_sub6,N_UE_sub6]) + p(k)*reshape(D_sub6_mmW(k,i,:,:),[N_UE_sub6,N_UE_mmW])*reshape(D_sub6_mmW(k,i,:,:),[N_UE_sub6,N_UE_mmW])';
         end
         for i = 1:K-K_mmW
+            for l = 1:L
+                H = reshape(H_sub6((l-1)*N+1:l*N,n,:,k), [N,N_UE_sub6]);
+                Hhat = reshape(Hhat_sub6((l-1)*N+1:l*N,n,:,k), [N,N_UE_sub6]);
+                H_int = reshape(H_sub6((l-1)*N+1:l*N,n,:,i), [N,N_UE_sub6]);
+                Hhat_int = reshape(Hhat_sub6((l-1)*N+1:l*N,n,:,i), [N,N_UE_sub6]);
+                D_sub6_sub6(k,i,:,:) = reshape(D_sub6_sub6(k,i,:,:),[N_UE_sub6,N_UE_sub6]) + H'*H_int;
+            end
             if (i~=k)
-                for l = 1:L
-                    H = reshape(H_sub6((l-1)*N+1:l*N,n,:,k), [N,N_UE_sub6]);
-                    Hhat = reshape(Hhat_sub6((l-1)*N+1:l*N,n,:,k), [N,N_UE_sub6]);
-                    H_int = reshape(H_sub6((l-1)*N+1:l*N,n,:,i), [N,N_UE_sub6]);
-                    Hhat_int = reshape(Hhat_sub6((l-1)*N+1:l*N,n,:,i), [N,N_UE_sub6]);
-                    D_sub6_sub6(k,i,:,:) = reshape(D_sub6_sub6(k,i,:,:),[N_UE_sub6,N_UE_sub6]) + H'*H_int;
-                end
                 Psi_sub6 (k,:,:) = reshape(Psi_sub6(k,:,:),[N_UE_sub6,N_UE_sub6]) + p(k)*reshape(D_sub6_sub6(k,i,:,:),[N_UE_sub6,N_UE_sub6])*reshape(D_sub6_sub6(k,i,:,:),[N_UE_sub6,N_UE_sub6])';
             end
         end
+        Psi_sub6_2(k,:,:) = inv(reshape(Psi_sub6(k,:,:),[N_UE_sub6,N_UE_sub6])) + reshape(D_sub6_sub6(k,k,:,:),[N_UE_sub6,N_UE_sub6])*reshape(D_sub6_sub6(k,k,:,:),[N_UE_sub6,N_UE_sub6])';
     end            
 end
 bk = reshape(mean(chgain_arr,1),[L,K]);
