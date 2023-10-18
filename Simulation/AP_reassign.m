3a263fa880a2c7fb73de72fd607f08be150b3044
sdatta97 <sdatta@nyu.edu>
2023-10-18 20:04:16 +0530

    adding loss percentage code for AP reassignment

--- Simulation/AP_reassign.m
+++ Simulation/AP_reassign.m
@@ -4,18 +4,25 @@ M = size(chgains,1);
 K = size(chgains,2);
 ap_idxs = find(D(:,k_idx));
 % for k = 1:(K-K_mmW)
+% for kk_idx = 1:length(ue_idxs)
+%     k = ue_idxs(kk_idx);
+%     ap_idxs_k = find(D(:,k));
+%     other_ap_idxs = setdiff(1:M,union(ap_idxs,ap_idxs_k));
+%     [~,other_ap_idxs_idxs] = sort(chgains(other_ap_idxs,k),'descend'); 
+%     other_ap_idxs = other_ap_idxs(other_ap_idxs_idxs);
+%     ap_idxs_affected = intersect(ap_idxs,ap_idxs_k);
+%     for m = 1:length(ap_idxs_affected)
+%         D_after_handoff(other_ap_idxs(m),k) = 1;
+%         if (p_fac == 1)
+%             D_after_handoff(ap_idxs_affected(m),k) = 0;
+%         end
+%     end
+% end
+loss_pc_arr = zeros(size(ue_idxs));
 for kk_idx = 1:length(ue_idxs)
     k = ue_idxs(kk_idx);
-    ap_idxs_k = find(D(:,k+K_mmW));
-    other_ap_idxs = setdiff(1:M,union(ap_idxs,ap_idxs_k));
-    [~,other_ap_idxs_idxs] = sort(chgains(other_ap_idxs,k+K_mmW),'descend'); 
-    other_ap_idxs = other_ap_idxs(other_ap_idxs_idxs);
+    ap_idxs_k = find(D(:,k));
     ap_idxs_affected = intersect(ap_idxs,ap_idxs_k);
-    for m = 1:length(ap_idxs_affected)
-        D_after_handoff(other_ap_idxs(m),k+K_mmW) = 1;
-        if (p_fac == 1)
-            D_after_handoff(ap_idxs_affected(m),k+K_mmW) = 0;
-        end
-    end
+    loss_pc_arr(kk_idx) = 100*(sum(chgains(ap_idxs_affected,kk_idx))/sum(chgains(:,kk_idx)));
 end
 end
