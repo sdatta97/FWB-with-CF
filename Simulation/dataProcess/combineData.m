515344118c66e23f4c828a28d4c8381a716445fd
sdatta97 <sdatta@nyu.edu>
2024-01-25 19:57:29 +0530

    add summary and data summarization script

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -1,7 +1,7 @@
 %Taken from 
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
-projectdir = './../resultData/outageResults_new';
+projectdir = 'C:/Users/dsoum/Desktop/outageData/outageResults100';
 dinfo = dir(fullfile(projectdir, '*.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
@@ -22,11 +22,11 @@ colNames = combinedTable.Properties.VariableNames;
 % for i=1:(length(colNames)-6)
 %     changingVars{i} = colNames{i};
 % end
-changingVars = cell(1,length(colNames)-8);
-for i=1:(length(colNames)-8)
+changingVars = cell(1,length(colNames)-4);
+for i=1:(length(colNames)-4)
     changingVars{i} = colNames{i};
 end
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std'});
 
-writetable(summaryTable,'./resultJun29.txt')
-writetable(summaryTable,'./resultJun29.csv')
+writetable(summaryTable,'./lambda_UE_100.txt')
+writetable(summaryTable,'./lambda_UE_100.csv')
