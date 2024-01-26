a6d9cc2e3ed9512635c5c156ff1b614f79b845be
sdatta97 <sdatta@nyu.edu>
2024-01-26 11:55:57 +0530

    modify scripts to run without quadriga - bug fix

--- Simulation/discreteSimulator_MIMO.m
+++ Simulation/discreteSimulator_MIMO.m
@@ -333,8 +333,8 @@ while nextEventTime < params.simTime
 %                             params.p_fac = p_fac;
 %                         end
                         sub6ConnectionState(ue_idx) = 1;
-%                         rate_dl = compute_link_rates_MIMO(params,channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW,ue_idx,sub6ConnectionState);                                              
-                        rate_dl = compute_link_rates_MIMO_quadriga(params,link,ue_idx,sub6ConnectionState);                                              
+                        rate_dl = compute_link_rates_MIMO(params,channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW,ue_idx,sub6ConnectionState);                                              
+%                         rate_dl = compute_link_rates_MIMO_quadriga(params,link,ue_idx,sub6ConnectionState);                                              
 %                         if ((rate_dl(ue_idx) >= r_min(ue_idx)) && all(rate_dl(1+numUE:numUE+numUE_sub6) >= r_min_sub6))
 %                         if rate_dl(ue_idx) >= r_min(ue_idx)
                         if rate_dl(ue_idx) >= r_min(ue_idx) && (mean(rate_dl_before_handoff(ue_idxs(2:end)) - rate_dl(ue_idxs(2:end))) <= rate_reduce_threshold)
