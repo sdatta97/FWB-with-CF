b9f994476837522a4704f661fb732ba571a9d857
sdatta97 <sdatta@nyu.edu>
2024-02-02 00:24:21 +0530

    adding data summarization

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -1,7 +1,7 @@
 %Taken from 
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
-projectdir = 'C:/Users/dsoum/Desktop/outageData/outageResults0.01_wo_quadriga_rmin_5';
+projectdir = 'C:/Users/dsoum/Desktop/impactData/impactData_50';
 dinfo = dir(fullfile(projectdir, '*.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
@@ -14,19 +14,19 @@ combinedTable = vertcat(tables{:});
 
 colNames = combinedTable.Properties.VariableNames;
 %% group the same parameter iterations and get stats mean and std, can add other stuff
-% changingVars = cell(1,length(colNames)-2);
-% for i=1:(length(colNames)-2)
-%     changingVars{i} = colNames{i};
-% end
+changingVars = cell(1,length(colNames)-2);
+for i=1:(length(colNames)-2)
+    changingVars{i} = colNames{i};
+end
 % changingVars = cell(1,length(colNames)-6);
 % for i=1:(length(colNames)-6)
 %     changingVars{i} = colNames{i};
 % end
-changingVars = cell(1,length(colNames)-4);
-for i=1:(length(colNames)-4)
-    changingVars{i} = colNames{i};
-end
+% changingVars = cell(1,length(colNames)-4);
+% for i=1:(length(colNames)-4)
+%     changingVars{i} = colNames{i};
+% end
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std'});
 
-writetable(summaryTable,'./lambda_UE_50_ant_64_bl_0.01_rmin_5e8.txt')
-writetable(summaryTable,'./lambda_UE_50_ant_64_bl_0.01_rmin_5e8.csv')
+writetable(summaryTable,'./impact_lambda_UE_50.txt')
+writetable(summaryTable,'./impact_lambda_UE_50.csv')
