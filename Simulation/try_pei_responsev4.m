1c306036e823fe718cb341cbc907e7aa937c3ee6
sdatta97 <sdatta@nyu.edu>
2024-02-05 22:56:45 +0530

    adding new sim code and plots

--- Simulation/try_pei_responsev4.m
+++ Simulation/try_pei_responsev4.m
@@ -55,8 +55,8 @@ params.rho_tot = 200;
 % rho_tot_arr = [10:10:100, 200:100:1000, 2000:1000:10000];
 
 %Power factor division
-% p_fac_arr = [1:1:10, 10:10:100]; %10.^(0:1:5);
-params.p_fac = 10;
+p_fac_arr = [1:1:10, 10:10:100]; %10.^(0:1:5);
+% params.p_fac = 10;
 
 %Prepare to save simulation results
 
@@ -97,11 +97,11 @@ params.N_UE_sub6 = 4;
 rmin_arr = 4*10^8;
 % params.r_min = rmin*rand(params.numUE,1);
 % lambda_BS = 50:50:200;%densityBS
-lambda_BS = 25;
+lambda_BS = 1; %25;
 % num_BS_arr = [2,5,10,20]; %densityBS
 % numUE_sub6_arr = 2:2:10;
 % numUE_sub6_arr = 10;
-lambda_UE_sub6 = 10:10:50;
+lambda_UE_sub6 = 2; %10:10:50;
 % for idxnumUEsub6 = 1:length(numUE_sub6_arr)
 for idxBSDensity = 1:length(lambda_BS)
     %% gNB locations
@@ -211,86 +211,89 @@ for idxBSDensity = 1:length(lambda_BS)
             % protocolParams.signalingAfterRachTime = [5 10 20]*10^(-3);
             protocolParams.signalingAfterRachTime = 0; %20*10^(-3);
             
-            
-            %% protocol params from other paper
-            frac = (mean(params.hb)-params.hr)/(params.ht-params.hr);
-            protocolParams.theta = 2*params.V.*params.lambdaBlockers*frac/pi;
-            % density_limits = [30,40,50,60,70];      % up to how many BS in coverage area, it will be ok since poisson distr.
-            % K_list = [1,2,3,4];                      % Degree of Connectivity
-            % protocolParams.omega_list = 1./protocolParams.connection_time;
-            % self_blockage = 5/6;
-           
-            N = params.num_antennas_per_gNB;  % antennas per AP
-            L = params.numGNB_sub6;
-            K = params.numUE + params.numUE_sub6;  % --Ground UEs
-            snr_db = params.snr_db;
-            LOOP = length(params.snr_db);
-            asd_length = length(params.ASD_VALUE);
-            hi_length = length(params.Kt_Kr_vsUE);
-            ASD_VALUE = params.ASD_VALUE;
-            ASD_CORR = params.ASD_CORR;
-            Kt_Kr_vsUE = params.Kt_Kr_vsUE;
-            K_Factor = params.K_Factor;
-            RAYLEIGH=params.RAYLEIGH;   %1= rayleigh, % 0=rician
-            Perf_CSI = params.Perf_CSI;
-            cov_area = params.cov_area;
-            %%
-            TAU_P_K_by_two = params.TAU_P_K_by_two;  
-            CH_estimation = params.CH_estimation;  
-            %%
-            LB = params.LB;  %Lower bound
-            UB = params.UB;  %Upper bound
-            no_of_rea = params.no_of_rea;     % no.of channel realizations
-            %%
-            pilot_pow = params.pilot_pow; 
-            noiseFigure = params.noiseFigure;
-            sigma_sf = params.sigma_sf;
-            Band = params.Band; %Communication bandwidth
-            tau_c = params.tau_c;      % coherence block length  
-            [gainOverNoisedB,R_gNB,R_ue_mmW,R_ue_sub6,pilotIndex,D,D_small,APpositions,UEpositions,distances] = generateSetup(params.numGNB,params.numGNB_sub6,params.numUE,params.numUE+params.numUE_sub6,params.num_antennas_per_gNB,params.N_UE_mmW,params.N_UE_sub6,params.coverageRange,params.coverageRange_sub6,params.tau_p,1,0,params.ASD_varphi,params.ASD_theta);
-            params.BETA = db2pow(gainOverNoisedB);   
-            params.D = D;
-            params.R_gNB = R_gNB;
-            params.R_ue_mmW = R_ue_mmW;
-            params.R_ue_sub6 = R_ue_sub6;
-            numUE = params.numUE;
-            sub6ConnectionState = zeros(numUE,1);
-            [channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW] = computePhysicalChannels_sub6_MIMO(params);
-            ue_idx = 1;
-            rate_dl_before_handoff = compute_link_rates_MIMO(params,channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW,ue_idx,sub6ConnectionState);                                              
-            sub6ConnectionState(ue_idx) = 1;
-            rate_dl_after_handoff = compute_link_rates_MIMO(params,channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW,ue_idx,sub6ConnectionState);                                              
-            numUE = params.numUE;
-            numUE_sub6 = params.numUE_sub6;
-            numBS = size(params.locationsBS,1);
-            numBlockers = params.numBlockers;
-            %% Recording the Results
-    
-            %Taking care of folder directory creation etc
-            dataFolder = 'resultData';
-            impactFolder = strcat(dataFolder,'/impactResults');
-            if not(isfolder(impactFolder))
-                mkdir(impactFolder)
+            for p_idx = 1:length(p_fac_arr)
+                params.p_fac = p_fac_arr(p_idx);
+                %% protocol params from other paper
+                frac = (mean(params.hb)-params.hr)/(params.ht-params.hr);
+                protocolParams.theta = 2*params.V.*params.lambdaBlockers*frac/pi;
+                % density_limits = [30,40,50,60,70];      % up to how many BS in coverage area, it will be ok since poisson distr.
+                % K_list = [1,2,3,4];                      % Degree of Connectivity
+                % protocolParams.omega_list = 1./protocolParams.connection_time;
+                % self_blockage = 5/6;
+               
+                N = params.num_antennas_per_gNB;  % antennas per AP
+                L = params.numGNB_sub6;
+                K = params.numUE + params.numUE_sub6;  % --Ground UEs
+                snr_db = params.snr_db;
+                LOOP = length(params.snr_db);
+                asd_length = length(params.ASD_VALUE);
+                hi_length = length(params.Kt_Kr_vsUE);
+                ASD_VALUE = params.ASD_VALUE;
+                ASD_CORR = params.ASD_CORR;
+                Kt_Kr_vsUE = params.Kt_Kr_vsUE;
+                K_Factor = params.K_Factor;
+                RAYLEIGH=params.RAYLEIGH;   %1= rayleigh, % 0=rician
+                Perf_CSI = params.Perf_CSI;
+                cov_area = params.cov_area;
+                %%
+                TAU_P_K_by_two = params.TAU_P_K_by_two;  
+                CH_estimation = params.CH_estimation;  
+                %%
+                LB = params.LB;  %Lower bound
+                UB = params.UB;  %Upper bound
+                no_of_rea = params.no_of_rea;     % no.of channel realizations
+                %%
+                pilot_pow = params.pilot_pow; 
+                noiseFigure = params.noiseFigure;
+                sigma_sf = params.sigma_sf;
+                Band = params.Band; %Communication bandwidth
+                tau_c = params.tau_c;      % coherence block length  
+                [gainOverNoisedB,R_gNB,R_ue_mmW,R_ue_sub6,pilotIndex,D,D_small,APpositions,UEpositions,distances] = generateSetup(params.numGNB,params.numGNB_sub6,params.numUE,params.numUE+params.numUE_sub6,params.num_antennas_per_gNB,params.N_UE_mmW,params.N_UE_sub6,params.coverageRange,params.coverageRange_sub6,params.tau_p,1,0,params.ASD_varphi,params.ASD_theta);
+                params.BETA = db2pow(gainOverNoisedB);   
+                params.D = D;
+                params.R_gNB = R_gNB;
+                params.R_ue_mmW = R_ue_mmW;
+                params.R_ue_sub6 = R_ue_sub6;
+                numUE = params.numUE;
+                sub6ConnectionState = zeros(numUE,1);
+                [channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW] = computePhysicalChannels_sub6_MIMO(params);
+                ue_idx = 1;
+                rate_dl_before_handoff = compute_link_rates_MIMO(params,channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW,ue_idx,sub6ConnectionState);                                              
+                sub6ConnectionState(ue_idx) = 1;
+                rate_dl_after_handoff = compute_link_rates_MIMO(params,channel_dl, channel_est_dl,channel_dl_mmW, channel_est_dl_mmW,ue_idx,sub6ConnectionState);                                              
+                numUE = params.numUE;
+                numUE_sub6 = params.numUE_sub6;
+                numBS = size(params.locationsBS,1);
+                numBlockers = params.numBlockers;
+                %% Recording the Results
+        
+                %Taking care of folder directory creation etc
+                dataFolder = 'resultData';
+                impactFolder = strcat(dataFolder,'/impactResults');
+                if not(isfolder(impactFolder))
+                    mkdir(impactFolder)
+                end
+                result_string = ['/handoff_impact_',num2str(numUE),...
+                    'UE_',num2str(lambda_BS(idxBSDensity)),...
+                    'lambdaBS_',num2str(lambda_UE_sub6(idxUEDensity)),...
+                    'lambdaUE_',num2str(numBlockers), 'Blockers_randomHeight_', num2str(aID),'Pow_factor_', num2str(params.p_fac)];    
+                %Since we are mostly interested in blockage probability, we want to
+                %transfer the data quickly to our local machine from server. We will save
+                %the results as a txt file.
+                recording_text_file_string = strcat(impactFolder,result_string,'.csv');
+                fileID = fopen(recording_text_file_string,'w');
+                output_categories = ['UE idx,','lambdaBS,','lambdaUE,','numBlockers,',...
+                    'powFactor,','rate_before_handoff,','rate_after_handoff,','rate_mmW\n'];  
+                fprintf(fileID,output_categories);
+                min_rate_req = params.r_min(1);   
+                mean_rate_before_handoff = mean(rate_dl_before_handoff(2:end));
+                mean_rate_after_handoff = mean(rate_dl_after_handoff(2:end)); 
+                rate_mmW = rate_dl_after_handoff (1);
+                formatSpec = '%d,%d,%d,%d,%d,%.16f,%.16f,%.16f\n';
+                fprintf(fileID,formatSpec,ue_idx, lambda_BS(idxBSDensity),lambda_UE_sub6(idxUEDensity),numBlockers,...
+                   params.p_fac,mean_rate_before_handoff,mean_rate_after_handoff,rate_mmW);                      
+                fclose(fileID);
             end
-            result_string = ['/handoff_impact_',num2str(numUE),...
-                'UE_',num2str(lambda_BS(idxBSDensity)),...
-                'lambdaBS_',num2str(lambda_UE_sub6(idxUEDensity)),...
-                'lambdaUE_',num2str(numBlockers), 'Blockers_randomHeight_', num2str(aID),'Min_rate', num2str(rmin)];    
-            %Since we are mostly interested in blockage probability, we want to
-            %transfer the data quickly to our local machine from server. We will save
-            %the results as a txt file.
-            recording_text_file_string = strcat(impactFolder,result_string,'.csv');
-            fileID = fopen(recording_text_file_string,'w');
-            output_categories = ['UE idx,','lambdaBS,','lambdaUE,','numBlockers,',...
-                'rate_before_handoff,','rate_after_handoff\n'];  
-            fprintf(fileID,output_categories);
-            min_rate_req = params.r_min(1);   
-            mean_rate_before_handoff = mean(rate_dl_before_handoff(2:end));
-            mean_rate_after_handoff = mean(rate_dl_after_handoff(2:end));                          
-            formatSpec = '%d,%d,%d,%d,%.16f,%.16f\n';
-            fprintf(fileID,formatSpec,ue_idx, lambda_BS(idxBSDensity),lambda_UE_sub6(idxUEDensity),numBlockers,...
-               mean_rate_before_handoff,mean_rate_after_handoff);                      
-            fclose(fileID);
         end
     end
 end
