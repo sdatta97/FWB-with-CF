b26d0fb68ef6b4bb3696f045df406d8f22b0679d
sdatta97 <sdatta@nyu.edu>
2024-02-23 11:02:27 +0530

    adding plots and data

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -1,7 +1,7 @@
 %Taken from 
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
-projectdir = 'C:/Users/dsoum/Desktop/impactData/impactData_pf_9';
+projectdir = 'C:/Users/dsoum/Desktop/outageData/outageResults0.1_Pow_fac_1000_rmin_5';
 dinfo = dir(fullfile(projectdir, '*.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
@@ -22,15 +22,15 @@ colNames = combinedTable.Properties.VariableNames;
 % for i=1:(length(colNames)-6)
 %     changingVars{i} = colNames{i};
 % end
-% changingVars = cell(1,length(colNames)-4);
-% for i=1:(length(colNames)-4)
-%     changingVars{i} = colNames{i};
-% end
-changingVars = cell(1,length(colNames)-3);
-for i=1:(length(colNames)-3)
+changingVars = cell(1,length(colNames)-4);
+for i=1:(length(colNames)-4)
     changingVars{i} = colNames{i};
 end
+% changingVars = cell(1,length(colNames)-3);
+% for i=1:(length(colNames)-3)
+%     changingVars{i} = colNames{i};
+% end
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std'});
 
-writetable(summaryTable,'./impact_pfac_9.txt')
-writetable(summaryTable,'./impact_pfac_9.csv')
+writetable(summaryTable,'./impact_pfac_1000_rmin_5.txt')
+writetable(summaryTable,'./impact_pfac_1000_rmin_5.csv')
