c75ce2b87d7f8fc2af602fd4715b33bef126eb98
sdatta97 <sdatta@nyu.edu>
2024-03-04 21:09:48 +0530

    add analysis parts

--- Simulation/CF_outage_analytical.m
+++ Simulation/CF_outage_analytical.m
@@ -13,8 +13,8 @@ function p_out = CF_outage_analytical(params,ue_idx,lambda_BS,lambda_UE)
 %     H = hypergeom([1,2],1,-0.1);
     hfunc = @(b,N,r1,r2) ((d0^2)/(b^N*(mu*N+2))*(hypergeom([N,N+(2/mu)],N+(2/mu)+1,-1/(b*(1+r2/d0)^(-mu)))/((1+r2/d0)^(-mu*(N+2/mu)))- hypergeom([N,N+(2/mu)],N+(2/mu)+1,-1/(b*(1+r1/d0)^(-mu)))/((1+r1/d0)^(-mu*(N+2/mu))))...
         - (d0^2)/(b^N*(mu*N+1))*(hypergeom([N,N+(1/mu)],N+(1/mu)+1,-1/(b*(1+r2/d0)^(-mu)))/((1+r2/d0)^(-mu*(N+1/mu)))- hypergeom([N,N+(1/mu)],N+(1/mu)+1,-1/(b*(1+r1/d0)^(-mu)))/((1+r1/d0)^(-mu*(N+1/mu)))));
-    term1 = @(b1,n1,N1,b2,N2,r1,r2) (b1^n1*hfunc(b1,n1,r1,r2)*((-1)^(N1-n1)*(factorial(N2+N1-n1-1)/factorial(N2-1))*(b2^-1-b1^-1)^(-N2-(N1-n1)))/(factorial(N1-n1)));
-    term2 = @(b1,N1,b2,n2,N2,r1,r2) (b2^n2*hfunc(b2,n2,r1,r2)*((-1)^(N2-n2)*(factorial(N1+N2-n2-1)/factorial(N1-1))*(b1^-1-b2^-1)^(-N1-(N2-n2)))/(factorial(N2-n2)));
+    term1 = @(b1,n1,N1,b2,N2,r1,r2) (b1^n1*hfunc(b1,n1,r1,r2)*((-1)^(N1-n1)*(factorial(N2+N1-n1-1)/factorial(N2-1))*((1/b2)-(1/b1))^(-N2-(N1-n1)))/(factorial(N1-n1)));
+    term2 = @(b1,N1,b2,n2,N2,r1,r2) (b2^n2*hfunc(b2,n2,r1,r2)*((-1)^(N2-n2)*(factorial(N1+N2-n2-1)/factorial(N1-1))*((1/b1)-(1/b2))^(-N1-(N2-n2)))/(factorial(N2-n2)));
     jfunc = @(b1,N1,b2,N2,r1,r2) ((sum(term1(b1,1:1:N1,N1,b2,N2,r1,r2)) + sum(term2(b1,N1,b2,1:1:N2,r1,r2)))/(b1^N1*b2^N2));
     Ns = 1+pi*lambda_BS*(R^2-r0^2);
     N_bar = ceil(lambda_UE*pi*R^2);
