46faf31671e7f2584278fd41c3cefb671f8f4d7e
sdatta97 <sdatta@nyu.edu>
2024-03-26 18:50:48 +0530

    adding data

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -1,7 +1,7 @@
 %Taken from 
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
-projectdir = 'C:/Users/dsoum/Desktop/impactData/impactData_pf_100_10/lambda_30';
+projectdir = 'C:/Users/dsoum/Desktop/impactData/impactData_pf_1000_ue_10_fdm';
 dinfo = dir(fullfile(projectdir, '*.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
@@ -33,5 +33,5 @@ for i=1:(length(colNames)-3)
 end
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std'});
 
-writetable(summaryTable,'./impact_pfac_100_10_ue_density_30_uc.txt')
-writetable(summaryTable,'./impact_pfac_100_10_ue_density_30_uc.csv')
+writetable(summaryTable,'./impact_pfac_1000_ue_density_100_fdm.txt')
+writetable(summaryTable,'./impact_pfac_1000_ue_density_100_fdm.csv')
