0edccf91e205c3de777a3aca877f638f807a7efa
sdatta97 <sdatta@nyu.edu>
2024-06-04 17:57:48 +0530

    add summary data

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -1,9 +1,9 @@
 %Taken from 
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
-% projectdir = 'C:\Users\dsoum\Desktop\data\outageData\outageResultspowfactor10_varyUEdensity_multiUE';
-projectdir = 'C:\Users\dsoum\FWB-with-CF\Simulation\resultData\impactResults\';
-dinfo = dir(fullfile(projectdir, '*.csv'));   %use appropriate extension
+projectdir = 'C:\Users\dsoum\Desktop\data\outageData\outageResults_varynumUE_ue_density_1000_multiUE';
+% projectdir = 'C:\Users\dsoum\FWB-with-CF\Simulation\resultData\impactResults\';
+dinfo = dir(fullfile(projectdir, 'results_8UE*.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
 tables = cell(nfiles,1);
@@ -15,19 +15,19 @@ combinedTable = vertcat(tables{:});
 
 colNames = combinedTable.Properties.VariableNames;
 %% group the same parameter iterations and get stats mean and std, can add other stuff
-changingVars = cell(1,length(colNames)-2);
-for i=1:(length(colNames)-2)
-    changingVars{i} = colNames{i};
-end
+% changingVars = cell(1,length(colNames)-2);
+% for i=1:(length(colNames)-2)
+%     changingVars{i} = colNames{i};
+% end
 % 
 % changingVars = cell(1,length(colNames)-6);
 % for i=1:(length(colNames)-6)
 %     changingVars{i} = colNames{i};
 % end
-% changingVars = cell(1,length(colNames)-4);
-% for i=1:(length(colNames)-4)
-%     changingVars{i} = colNames{i};
-% end
+changingVars = cell(1,length(colNames)-4);
+for i=1:(length(colNames)-4)
+    changingVars{i} = colNames{i};
+end
 % changingVars = cell(1,length(colNames)-6);
 % for i=1:(length(colNames)-6)
 %     changingVars{i} = colNames{i};
@@ -39,5 +39,5 @@ end
 
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std'});
 
-writetable(summaryTable,'./impact_vary_ue_density_multi_UEv2.txt')
-writetable(summaryTable,'./impact_vary_ue_density_multi_UEv2.csv')
+writetable(summaryTable,'./outage_8UE_multi_UEv2.txt')
+writetable(summaryTable,'./outage_8UE_multi_UEv2.csv')
