d8168d6b50dafba005a434de67f670c1d0a596ec
sdatta97 <sdatta@nyu.edu>
2024-08-12 13:25:34 -0400

    adding summary data, updating code to vary BS density

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -2,7 +2,7 @@
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
 % projectdir = '/Users/sdatta/FWB-with-CF/Simulation/resultData/impactResults';
-projectdir = '/Users/sdatta/Desktop/data/multiUEsqgridBS/outageResults';
+projectdir = '/Users/sdatta/Desktop/data/multiUEsqgridBS_small/outageResults';
 dinfo = dir(fullfile(projectdir, '*_1000l*.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
@@ -30,5 +30,5 @@ end
 
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std','median'});
 
-writetable(summaryTable,'./outage_multi_UE_sqgridBS_fr1_ue_density_1000.txt')
-writetable(summaryTable,'./outage_multi_UE_sqgridBS_fr1_ue_density_1000.csv')
+writetable(summaryTable,'./outage_multi_UE_sqgridBS_fr1_ue_density_1000_small.txt')
+writetable(summaryTable,'./outage_multi_UE_sqgridBS_fr1_ue_density_1000_small.csv')
