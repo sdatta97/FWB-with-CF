f8bf419cc32c649180ea2e28d492b50fcf551591
sdatta97 <sdatta@nyu.edu>
2024-09-03 12:01:13 -0400

    adding data and plots

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -2,8 +2,8 @@
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
 % projectdir = '/Users/sdatta/FWB-with-CF/Simulation/resultData/impactResults';
-projectdir = '/Users/sdatta/Desktop/data/multiUEsqgridBS_varyBSdensity/lambda_100';
-dinfo = dir(fullfile(projectdir, '*.csv'));   %use appropriate extension
+projectdir = '/Users/sdatta/Desktop/data/multiUEsqgridBS_vary_l_lambda_UE_250/outageResults';
+dinfo = dir(fullfile(projectdir, '*s100.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
 tables = cell(nfiles,1);
@@ -30,5 +30,5 @@ end
 
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std','median'});
 
-writetable(summaryTable,'./outage_multi_UE_sqgridBS_bs_density_100.txt')
-writetable(summaryTable,'./outage_multi_UE_sqgridBS_bs_density_100.csv')
+writetable(summaryTable,'./outage_multi_UE_sqgridBS_lambda_UE_250_l_100.txt')
+writetable(summaryTable,'./outage_multi_UE_sqgridBS_lambda_UE_250_l_100.csv')
