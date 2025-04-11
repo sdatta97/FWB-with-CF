c2019a8b24cdfed55d0269bb2b1bf6f123d9805a
sdatta97 <sdatta@nyu.edu>
2025-04-11 16:10:06 -0400

    code with varying BS density for 250 UE density

--- Simulation/dataProcess/combineData.m
+++ Simulation/dataProcess/combineData.m
@@ -1,8 +1,9 @@
 %Taken from 
 % https://in.mathworks.com/matlabcentral/answers/
 %   538119-how-to-import-to-matlab-many-text-files-as-table-type
-projectdir = '//Users/sdatta/FWB-with-CF/Simulation/resultData/ppResults';
+% projectdir = '//Users/sdatta/FWB-with-CF/Simulation/resultData/ppResults';
 % projectdir = '/Users/sdatta/Desktop/data/multiUEsqgridBS_vary_l_lambda_UE_500/outageResults';
+projectdir = '/Users/sdatta/outage_data_modify/outageResults';
 dinfo = dir(fullfile(projectdir, '*.csv'));   %use appropriate extension
 filenames = fullfile({dinfo.folder}, {dinfo.name});
 nfiles = length(filenames);
@@ -19,10 +20,10 @@ colNames = combinedTable.Properties.VariableNames;
 % for i=1:(length(colNames)-6)
 %     changingVars{i} = colNames{i};
 % end
-% changingVars = cell(1,length(colNames)-4);
-% for i=1:(length(colNames)-4)
-%     changingVars{i} = colNames{i};
-% end
+changingVars = cell(1,length(colNames)-4);
+for i=1:(length(colNames)-4)
+    changingVars{i} = colNames{i};
+end
 % changingVars = cell(1,length(colNames)-5);
 % for i=1:(length(colNames)-5)
 %     changingVars{i} = colNames{i};
@@ -31,12 +32,12 @@ colNames = combinedTable.Properties.VariableNames;
 % for i=1:(length(colNames)-2)
 %     changingVars{i} = colNames{i};
 % end
-changingVars = cell(1,length(colNames)-1);
-for i=1:(length(colNames)-1)
-    changingVars{i} = colNames{i};
-end
+% changingVars = cell(1,length(colNames)-1);
+% for i=1:(length(colNames)-1)
+%     changingVars{i} = colNames{i};
+% end
 
 summaryTable  = groupsummary(combinedTable,changingVars,{'mean','std','median'});
 
-writetable(summaryTable,'./pp_data_vary_ue_density_fr2pp.txt')
-writetable(summaryTable,'./pp_data_vary_ue_density_fr2pp.csv')
+writetable(summaryTable,'./outage_data_lambda_BS_vary_modify.txt')
+writetable(summaryTable,'./outage_data_lambda_BS_vary_modify.csv')
